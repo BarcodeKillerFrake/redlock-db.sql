dff22a7e1b6a317717d66318ae6e42c2c496f47a
barcodekiller <wibusederhana123@gmail.com>
2023-05-01 23:19:07 +0700

    update

--- index.php
+++ index.php
@@ -3,17 +3,20 @@ $servername = "mysql";
 $username = "root";
 $password = "password";
 $dbname = "redlock";
+$count = 0;
 
 $conn = new mysqli($servername, $username, $password);
 
 $conn->select_db($dbname);
 
-$sql = "SELECT ID, Nama, Alamat, Jabatan FROM users";
+$sql = "SELECT * FROM users";
 $result = $conn->query($sql);
 
 while($row = $result->fetch_assoc()) {
         echo "ID: ". $row["ID"]. " - Nama: ". $row["Nama"]. " - Alamat " . $row["Alamat"]. " - Jabatan " . $row["Jabatan"]. "<br>";
+        $count++;
     }
+    echo $count;
 
 $conn->close();
 ?>
